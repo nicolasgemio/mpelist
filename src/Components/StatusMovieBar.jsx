8302c6c8f016e04cd95a1ab8793cb5947bd26c4e
nicolasgemio <nicolasgemio@users.noreply.github.com>
2022-07-28 04:32:19 -0300

    Estilos de Barra de status

--- src/Components/StatusMovieBar.jsx
+++ src/Components/StatusMovieBar.jsx
@@ -7,17 +7,17 @@ export function StatusMovieBar(props){
     };
 
     return (
-      <div className="btn-group px-5" role="group" aria-label="Basic radio toggle button group">
-        <input type="radio" className="btn-check" name="btnradio" id="btnradio1" autoComplete="off" defaultChecked onChange={() => changeStatus(0)}/>
-        <label className="btn btn-outline-primary" htmlFor="btnradio1">Todas</label>
+      <div className="btn-group px-5 statusBarStyle" role="group" aria-label="radio toggle button group">
+        <input type="radio" className="btn-check statusBarStyle" name="btnradio" id="btnradio1" autoComplete="off" defaultChecked onChange={() => changeStatus(0)}/>
+        <label className="btn btn-outline-primary statusBarStyle" htmlFor="btnradio1">Todas</label>
         
         <input type="radio" className="btn-check" name="btnradio" id="btnradio2" autoComplete="off" onChange={() => changeStatus(1)}/>
-        <label className="btn btn-outline-primary" htmlFor="btnradio2">Pendientes</label>
+        <label className="btn btn-outline-primary statusBarStyle" htmlFor="btnradio2">Pendientes</label>
         
         <input type="radio" className="btn-check" name="btnradio" id="btnradio3" autoComplete="off" onChange={() => changeStatus(2)}/>
-        <label className="btn btn-outline-primary" htmlFor="btnradio3">Viendo</label>
+        <label className="btn btn-outline-primary statusBarStyle" htmlFor="btnradio3">Viendo</label>
         
         <input type="radio" className="btn-check" name="btnradio" id="btnradio4" autoComplete="off" onChange={() => changeStatus(3)}/>
-        <label className="btn btn-outline-primary" htmlFor="btnradio4">Vistas</label>
+        <label className="btn btn-outline-primary statusBarStyle" htmlFor="btnradio4">Vistas</label>
 </div>)
 }
